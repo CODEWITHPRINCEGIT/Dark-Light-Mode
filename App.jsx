d9ec121487937581f563bc6ceb7d7170a6a2d421
Prince Kumar Jain <princemoditkg@gmail.com>
2024-12-12 16:27:00 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import Usercard from "./assets/Card/Usercard";
+import Toogle from "./assets/Toggle Button/toggle";
+
+
+
+function App() {
+  return (
+    <>
+    <Toogle/>
+    </>
+
+  );
+}
+
+export default App
